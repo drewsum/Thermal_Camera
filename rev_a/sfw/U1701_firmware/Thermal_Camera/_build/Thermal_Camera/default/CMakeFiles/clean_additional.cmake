15bb43c323f65272507e941e35c1a4f95023e245
Drew Maatman <drewmaatman@gmail.com>
2026-07-17 22:24:19 -0500

    updated gitignore, added gpio port status function

--- rev_a/sfw/U1701_firmware/Thermal_Camera/_build/Thermal_Camera/default/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
-  file(REMOVE_RECURSE
-  "C:\\Users\\drewm\\Documents\\KiCad Projects\\Projects\\Thermal_Camera\\rev_a\\sfw\\U1701_firmware\\Thermal_Camera\\out\\Thermal_Camera"
-  )
-endif()
