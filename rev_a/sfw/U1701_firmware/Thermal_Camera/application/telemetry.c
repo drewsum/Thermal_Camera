5ed06f707b0f24af01ad09b610139095602ad5da
Drew Maatman <drewmaatman@gmail.com>
2026-07-19 13:20:00 -0500

    Rename some telemetry printout stuff

--- rev_a/sfw/U1701_firmware/Thermal_Camera/application/telemetry.c
+++ rev_a/sfw/U1701_firmware/Thermal_Camera/application/telemetry.c
@@ -96,9 +96,9 @@ void printCurrentTelemetry(void) {
     terminalTextAttributes(CYAN_COLOR, BLACK_COLOR, BOLD_FONT);
     printf("\tMiscellaneous Telemetry:\033[K\r\n");
     terminalTextAttributes(CYAN_COLOR, BLACK_COLOR, NORMAL_FONT);
-    printf("\t\tHost Die Temperature: %.3fC\033[K\r\n", telemetry.mcu_die_temp);
-    printf("\t\tHost ADC Reference Voltage: %.3fV\033[K\r\n", telemetry.adc_vref_voltage);
-    printf("\t\tHost Battery Voltage: %.3fV\033[K\r\n", telemetry.mcu_battery_voltage);
+    printf("\t\tMCU Die Temperature: %.3fC\033[K\r\n", telemetry.mcu_die_temp);
+    printf("\t\tMCU ADC Reference Voltage: %.3fV\033[K\r\n", telemetry.adc_vref_voltage);
+    printf("\t\tMCU Battery Voltage: %.3fV\033[K\r\n", telemetry.mcu_battery_voltage);
     printf("\t\tAmbient Temperature: %.3fC\033[K\r\n", telemetry.ambient_temperature);
     
     // print out state of PGOOD pins
